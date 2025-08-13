946f5ac3fea046b4718f859fc22ba5b0176d7658
Nagata Kana <ngtkana@gmail.com>
2025-08-14 02:16:52 +0900

    add overseer

---
+++ config/nvim/lua/plugins/overseer.lua
@@ -0,0 +1,8 @@
+return {
+    "stevearc/overseer.nvim",
+    keys = {
+        { "<leader>or", "<CMD>OverseerRun<CR>" },
+        { "<leader>ot", "<CMD>OverseerToggle<CR>" },
+    },
+    opts = {},
+}
